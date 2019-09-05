debb0b91609b7e598b8373f3c8bd910e2b29e8ea
Pluto She <plutoshe@gmail.com>
2019-09-04 20:56:50 -0600

    add binding data, and add env variables

--- Engine/Graphics/cEffect.cpp
+++ Engine/Graphics/cEffect.cpp
@@ -1,35 +1,52 @@
+#include "GraphicsEnv.h"
 #include "cEffect.h"
+#include "PlatformIncludes.h"
 
-void eae6320::Graphics::Effect::AddShader(eae6320::Graphics::ShaderEffect shader)
-{
-	m_shaders.push_back(shader);
-}
-//template <class T>
+#include <Engine/Asserts/Asserts.h>
+
+
+
 eae6320::cResult eae6320::Graphics::Effect::Load(eae6320::Assets::cManager<eae6320::Graphics::cShader> &manager, eae6320::Graphics::cShader::Handle &vertexShader, eae6320::Graphics::cShader::Handle &fragmentShader)
 {
 	auto result = eae6320::Results::Success;
-
-	for (size_t i = 0; i < m_shaders.size(); i++)
-	{
-		eae6320::Graphics::cShader::Handle *currentHandler = nullptr;
-		switch (m_shaders[i].m_shaderType)
-		{
-		case eae6320::Graphics::ShaderTypes::Vertex:
-			currentHandler = &vertexShader;
-			break;
-		case eae6320::Graphics::ShaderTypes::Fragment:
-			currentHandler = &fragmentShader;
-			break;
-		}
-		if (currentHandler == nullptr)
-		{
-			EAE6320_ASSERTF(false, "Load Shader failed");
-			return eae6320::Results::Failure;
-		}
-		if (currentHandler != nullptr && !(result = manager.Load(m_shaders[i].m_shaderPath, *currentHandler, m_shaders[i].m_shaderType))) {
-			EAE6320_ASSERTF(false, "Load Shader failed");
-			return result;
-		}
+	if (m_vertexShaderPath != "" && !(result = manager.Load(m_vertexShaderPath, vertexShader, eae6320::Graphics::ShaderTypes::Vertex))) {
+		EAE6320_ASSERTF(false, "Load Shader failed");
+		return result;
 	}
+	if (m_fragmentShaderPath != "" && !(result = manager.Load(m_fragmentShaderPath, fragmentShader, eae6320::Graphics::ShaderTypes::Fragment))) {
+		EAE6320_ASSERTF(false, "Load Shader failed");
+		return result;
+	}
+	
 	return result;
 }
+
+void eae6320::Graphics::Effect::Bind(eae6320::Assets::cManager<eae6320::Graphics::cShader>& s_manager, eae6320::Graphics::cShader::Handle& s_vertexShader, eae6320::Graphics::cShader::Handle& s_fragmentShader)
+{
+#if defined( EAE6320_PLATFORM_D3D )
+	auto* const direct3dImmediateContext = sContext::g_context.direct3dImmediateContext;
+	EAE6320_ASSERT(direct3dImmediateContext);
+	constexpr ID3D11ClassInstance* const* noInterfaces = nullptr;
+	constexpr unsigned int interfaceCount = 0;
+	// Vertex shader
+	{
+		EAE6320_ASSERT(s_vertexShader);
+		auto* const shader = cShader::s_manager.Get(s_vertexShader);
+		EAE6320_ASSERT(shader && shader->m_shaderObject.vertex);
+		direct3dImmediateContext->VSSetShader(shader->m_shaderObject.vertex, noInterfaces, interfaceCount);
+	}
+	// Fragment shader
+	{
+		EAE6320_ASSERT(s_fragmentShader);
+		auto* const shader = cShader::s_manager.Get(s_fragmentShader);
+		EAE6320_ASSERT(shader && shader->m_shaderObject.fragment);
+		direct3dImmediateContext->PSSetShader(shader->m_shaderObject.fragment, noInterfaces, interfaceCount);
+	}
+
+#elif defined( EAE6320_PLATFORM_GL )
+	EAE6320_ASSERT(eae6320::Graphics::Env::s_programId != 0);
+	glUseProgram(eae6320::Graphics::Env::s_programId);
+	EAE6320_ASSERT(glGetError() == GL_NO_ERROR);
+#endif
+}
+
--- Engine/Graphics/cEffect.h
+++ Engine/Graphics/cEffect.h
@@ -8,26 +8,17 @@ namespace eae6320
 {
 	namespace Graphics
 	{
-		class ShaderEffect
-		{
-		public:
-			std::string m_shaderPath;
-			ShaderTypes::eType m_shaderType;
-			ShaderEffect(std::string path, ShaderTypes::eType type) 
-			{
-				m_shaderPath = path;
-				m_shaderType = type;
-			}
-		};
-
 		class Effect
 		{
 		public:
-			std::vector<ShaderEffect> m_shaders;
-			Effect() { m_shaders.clear(); }
-			~Effect() { m_shaders.clear(); }
-			void AddShader(ShaderEffect shader);
-			eae6320::cResult Load(eae6320::Assets::cManager<eae6320::Graphics::cShader> &manager, eae6320::Graphics::cShader::Handle &vertexShader, eae6320::Graphics::cShader::Handle &fragmentShader);
+			std::string m_vertexShaderPath;
+			std::string m_fragmentShaderPath;
+			Effect() { m_vertexShaderPath = ""; m_fragmentShaderPath = ""; }
+			~Effect() {}
+			void SetVertexShaderPath(std::string vertexShaderPath) { m_vertexShaderPath = vertexShaderPath; }
+			void SetFragmentShaderPath(std::string fragmentShaderPath) { m_fragmentShaderPath = fragmentShaderPath; }
+			eae6320::cResult Load(eae6320::Assets::cManager<eae6320::Graphics::cShader>& manager, eae6320::Graphics::cShader::Handle& vertexShader, eae6320::Graphics::cShader::Handle& fragmentShader);
+			void Bind(eae6320::Assets::cManager<eae6320::Graphics::cShader>& s_manager, eae6320::Graphics::cShader::Handle& s_vertexShader, eae6320::Graphics::cShader::Handle& s_fragmentShader);
 		};
 	}
 }
